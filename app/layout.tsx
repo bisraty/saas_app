629f29e0b2351a49cdc23b989e58804c8fedf1b4
bisrat <bisrat2007manu@gmail.com>
2025-06-13 18:53:41 +0300

    feat: implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,7 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
+        <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/images/logo.svg",
+          },
+        }}
+        signUpForceRedirectUrl={"/"}
+      >
+      <body className={`${bricolage.variable} antialiased`}>
+        <Navbar />
+        {children}
+        </body>
+        </ClerkProvider>
     </html>
   );
 }
