71330a723bed3c2b92e596643721f3bf9bfe103c
bisrat <bisrat2007manu@gmail.com>
2025-06-16 16:18:16 +0300

    feat: add vapi Setup & Header

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -14,7 +14,7 @@ const NewCompanion = async () => {
 
     return (
         <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-            {canCreateCompanion ? (
+            {!canCreateCompanion ? (
                 <article className="w-full gap-4 flex flex-col">
                     <h1>Companion Builder</h1>
 
